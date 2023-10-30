2a1fff32c1d2bc2b7951571d230852cf396e3b1b
dmitry <dmitry.okhotnikov@gmail.com>
2023-10-30 20:11:01 +0300

    add product_page tests

--- final/pages/product_page.py
+++ final/pages/product_page.py
@@ -25,3 +25,4 @@ class ProductPage(BasePage):
         return basket_messages[2].text
 
 
+
--- final/test_product_page.py
+++ final/test_product_page.py
@@ -1,7 +1,8 @@
 from .pages.product_page import ProductPage
 
-#link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+# link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+
 
 def test_guest_can_add_product_to_basket(browser):
     page = ProductPage(browser, link)
@@ -9,4 +10,3 @@ def test_guest_can_add_product_to_basket(browser):
     page.add_product_to_basket()
     page.check_product_name()
     page.check_product_cost()
-
